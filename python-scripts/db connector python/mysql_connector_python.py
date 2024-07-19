9cc7b421b81cec4020f40f66c240d218eaa90184
mvoemel <michael@voemel.org>
2024-07-19 15:41:38 +0200

    FIX: changed credentials in mysql_connector_python

--- python-scripts/db connector python/mysql_connector_python.py
+++ python-scripts/db connector python/mysql_connector_python.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="mvoemel",
-    password="mvoemel",
+    host="YOUR_HOST",
+    user="YOUR_USERNAME",
+    password="YOUR_PASSWORD",
     auth_plugin="mysql_native_password"
     # database=""
 )
